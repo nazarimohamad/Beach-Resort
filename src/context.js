ce7535d21c32200b9282ad5fc386c8e073c60be7
Ryan Nazari <ryannazari21@gmail.com>
2020-01-19 14:29:06 +0200

    scroll to top on changing the routes

--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import items from "./data";
+// import items from "./data";
 import Client from "./Contentful";
 
 const RoomContext = React.createContext();
@@ -23,11 +23,11 @@ class RoomProvider extends Component {
 
   getData = async () => {
     try {
-      // let response = await Client.getEntries({
-      //   content_type: "beachResort",
-      //   order: "-fields.price",
-      // });
-      let rooms = this.formatData(items);
+      let response = await Client.getEntries({
+        content_type: "beachResort",
+        order: "-fields.price",
+      });
+      let rooms = this.formatData(response.items);
       let featuredRooms = rooms.filter(room => room.featured === true);
       let maxPrice = Math.max(...rooms.map(item => item.price));
       let maxSize = Math.max(...rooms.map(item => item.size));
